5aa8803bf33d8f62224fbead04e2ba3b6b8118c9
Winnn Hardjanto <mynameiswindha@gmail.com>
2022-05-20 18:11:09 +0700

    inv finished

--- inv_cari.php
+++ inv_cari.php
@@ -4,6 +4,15 @@ include_once "library/inc.connection.php";
 include_once "library/inc.library.php";
 //include_once "library/inc.seslogin.php"; 
 
+// pagination
+$p = $_GET["p"];
+
+
+if (is_null($p)){
+  $_GET["p"] = 1;
+echo "<meta http-equiv='refresh' content='0; url=inv_cari.php?p=1'>";
+}
+
 # PENCARIAN DATA
 $pencarianSQL	= "";
  
@@ -27,87 +36,165 @@ if(isset($_POST['btnCari'])) {
 $keyWord		= isset($_GET['keyWord']) ? $_GET['keyWord'] : '';
 $dataKataKunci 	= isset($_POST['txtKataKunci']) ? $_POST['txtKataKunci'] : $keyWord;
 
-# UNTUK PAGING (PEMBAGIAN HALAMAN)
-$baris 		= 50;
-$halaman 	= isset($_GET['hal']) ? $_GET['hal'] : 0;
-$pageSql 	= "SELECT * FROM inv $pencarianSQL";
-$pageQry 	= sqlsrv_query($koneksidb, $pageSql );
-$jmlData	= sqlsrv_num_rows($pageQry);
-$maksData	= ceil($jmlData/$baris);
+// pagination start
+// Limit data per page
+$limitdata = 10;
+
+/* It's for counting the rows. */
+$params = array();
+$options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
+$stmt = sqlsrv_query( $koneksidb, "SELECT * FROM inv" , $params, $options );
+
+$row_count = sqlsrv_num_rows( $stmt );
+
+
+$jumlahpage = ceil($row_count/$limitdata);
+
+
+
+$rowakhir = $p * $limitdata;
+// $rowawal = $rowakhir - 9;
+$rowawal	= $limitdata *   ($p-1);
+
+$datapageinv2 = "WITH
+            tbl_inv
+            AS
+            (
+                SELECT ROW_NUMBER() OVER (
+                          ORDER BY 
+                                  inv ASC
+                          ) row_num, * FROM inv $pencarianSQL
+            )
+        SELECT
+            *
+        FROM
+            tbl_inv
+        WHERE 
+                      row_num >= $rowawal AND
+            row_num <= $rowakhir;
+              ";
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Pencarian COA</title>
-<link href="../styles/style.css" rel="stylesheet" type="text/css"></head>
-<body>
-<h1> PENCARIAN INVENTORY </h1>
-<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" name="form1" target="_self">
-  <table class="table-list" width="800" border="0" cellspacing="2" cellpadding="3">
-    <tr>
-      <td width="114" bgcolor="#CCCCCC"><strong>PENCARIAN</strong></td>
-      <td width="6">&nbsp;</td>
-      <td width="654">&nbsp;</td>
-    </tr>
-    <tr>
-      <td><strong>Cari Kode./ Nama </strong></td>
-      <td><strong>:</strong></td>
-      <td><input name="txtKataKunci" type="text" id="txtKataKunci" value="<?php echo $dataKataKunci; ?>" size="40" maxlength="80">
-      <input name="btnCari" type="submit" id="btnCari" value="Cari"></td>
-    </tr>
-  </table>
-</form>
 
-<table class="table-list" width="800" border="0" cellspacing="2" cellpadding="3">
-  <tr>
-    <td width="25" bgcolor="#CCCCCC"><strong>No.</strong></td>
-    <td width="140" bgcolor="#CCCCCC"><strong>Kode Inventory </strong></td>
-    <td width="288" bgcolor="#CCCCCC"><strong>Nama Inventory </strong></td>
-    <td width="184" bgcolor="#CCCCCC"><strong>Unit</strong></td>
-    <td width="65" bgcolor="#CCCCCC"><strong>Harga Beli </strong></td>
-    <td width="65" bgcolor="#CCCCCC"><strong>Harga Jual </strong></td>
-    <td width="48" bgcolor="#CCCCCC"><strong>Tools</strong></td>
-  </tr>
-  
-<?php
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <title>Pencarian Inventory</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <h1 class="display-5">PENCARIAN INVENTORY</h1>
+    </div>
+    <div class="container-fluid">
+        <!-- search box -->
+        <div class="row">
+            <div class="col-2 mb-3">
+                <strong>PENCARIAN</strong>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2 mx-0">
+                <strong>Cari No./ Nama</strong>
+            </div>
+            <div class="col-4 mr-5">
+                <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" name="form1" target="_self">
+                    <div class="form-group mx-sm-3 mb-2">
+                        <label for="txtKataKunci" class="sr-only">Cari</label>
+                        <input type="text" class="form-control" id="txtKataKunci" name="txtKataKunci"
+                            placeholder="Cari...">
+                    </div>
+            </div>
+            <div class="col">
+                <button type="submit" name="btnCari" id="btnCari" class="btn btn-primary mb-2">Cari</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <table class="table table-bordered text-center table-hover mx-auto">
+            <thead class="thead-dark">
+                <tr>
+                    <th>No</th>
+                    <th>Kode Inventory</th>
+                    <th>Nama Inventory</th>
+                    <th>Unit</th>
+                    <th>Harga Beli</th>
+                    <th>Harga Jual</th>
+                    <th>Tools</th>
+                </tr>
+            </thead>
+            <?php
 // Skrip menampilkan data dari database
-$mySql = "SELECT * FROM inv $pencarianSQL ORDER BY inv ASC ";
-$myQry = sqlsrv_query($koneksidb, $mySql);
-$nomor  = 0; 
+$myQry = sqlsrv_query($koneksidb, $datapageinv2);
 while ($myData = sqlsrv_fetch_array($myQry)) {
-	$nomor++;
+	$nomor  = $myData['row_num']; 
 	$Kode = $myData['inv'];
 	
 	// gradasi warna
 	if($nomor%2==1) { $warna=""; } else {$warna="#F5F5F5";}
 ?>
-
-  <tr>
-    <td> <?php echo $nomor; ?> </td>
-    <td> <?php echo $myData['inv']; ?> </td>
-    <td> <?php echo $myData['name']; ?> </td>
-    <td> <?php echo $myData['unit']; ?> </td>
-    <td align="right"> <?php echo format_angka($myData['bprice']); ?> </td>
-    <td align="right"> <?php echo format_angka($myData['price1']); ?> </td>
-    <td> 
-	<a href="#" onClick="window.opener.document.getElementById('txtInv').value = '<?php echo $myData['inv']; ?>';
+            <tr>
+                <td> <?php echo $myData['row_num']; ?> </td>
+                <td> <?php echo $myData['inv']; ?> </td>
+                <td> <?php echo $myData['name']; ?> </td>
+                <td> <?php echo $myData['unit']; ?> </td>
+                <td> <?php echo format_angka($myData['bprice']); ?> </td>
+                <td> <?php echo format_angka($myData['price1']); ?> </td>
+                <td>
+                    <a href="#" onClick="window.opener.document.getElementById('txtInv').value = '<?php echo $myData['inv']; ?>';
 						 window.opener.document.getElementById('txtNamaInv').value = '<?php echo $myData['name']; ?>';
-						 window.close();"> <b>Pilih </b> </a>  </td>
-  </tr>
+						 window.close();"> <b>Pilih </b> </a>
+                </td>
+            </tr>
+            <?php } ?>
+            <tr>
+                <td><strong>Jumlah Data : </strong> <?php echo $row_count; ?>
+                </td>
+                <td colspan="3" align="right">
+                    <div class="col d-flex justify-content-center">
+                        <nav aria-label="Page navigation example">
+                            <!-- /* It's a pagination. */ -->
+                            <ul class="pagination">
+                                <?php 
 
-<?php } ?>
+                        $prev = $_GET["p"] - 1;
+                        $nxt = $_GET["p"] + 1;
 
-  <tr>
-    <td colspan="3"><strong>Jumlah Data : </strong> <?php echo $jmlData; ?> </td>
-    <td colspan="3" align="right"><strong>Halaman Ke : </strong> 
-	<?php
-	for ($h = 1; $h <= $maksData; $h++) {
-		$list[$h] = $baris * $h - $baris;
-		echo " <a href='inv_cari.php?hal=$list[$h]&keyWord=$dataKataKunci'>$h</a> ";
-	}
-	?>
-	</td>
-  </tr>
-</table>
+                        $nxtoff = "";
+                        $prevoff = "";
+                        
+                        if ($nxt > $jumlahpage) {
+                            $nxtoff = "disabled";
+                        }
+                        if ($_GET["p"] == 1) {
+                            $prevoff = "disabled";
+                        }
+                        
+
+                        echo '<li class="page-item '. $prevoff .'"><a class="page-link" href="inv_cari.php?p='.$prev.'">Previous</a></li>';
+
+
+                        for ($x = 1; $x <= $jumlahpage; $x+=1) {
+                            $active = ""; 
+                            if ($_GET["p"] == $x) {
+                                $active = "active";
+                            }            
+                            echo '<li class="page-item '.$active.'"><a class="page-link" href="inv_cari.php?p='.$x.'">';
+                        echo "$x";
+                        echo'</a></li>';
+                        }
+                        echo '<li class="page-item '. $nxtoff .'"><a class="page-link" href="inv_cari.php?p='. $nxt .'">Next</a></li>';?>
+                            </ul>
+                        </nav>
+                        <!-- end pagination -->
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
 </body>
+
 </html>
